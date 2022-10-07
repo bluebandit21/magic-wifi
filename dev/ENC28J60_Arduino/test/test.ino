125e4fd793f41b9d3ff7e7c8c2514fe40ee86922
Andrew Plotner <bluebandit21@gmail.com>
2022-10-06 21:20:23 -0400

    Add in simple code for sending out raw frame

--- dev/ENC28J60_Arduino/test/test.ino
+++ dev/ENC28J60_Arduino/test/test.ino
@@ -18,6 +18,14 @@ void setup() {
     ether.enableBroadcast(false);
 }
 
+const byte broadcastEth[] = {
+    0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, //Broadcast Eth MAC dest
+    0x74, 0x69, 0x69, 0x2D, 0x30, 0x31, //Source Eth MAC addr (copied from above)
+    0x00, 0x05,                         //Length (five bytes)
+    'H', 'e', 'l', 'l', 'o'
+};
+
+
 void loop() {
   // put your main code here, to run repeatedly:
     int len = ether.packetReceive();
@@ -31,5 +39,9 @@ void loop() {
     }
     Serial.println("");
     Serial.println("End hexdump.");
+    delay(1000);
+    memcpy(Ethernet::buffer, broadcastEth, sizeof(broadcastEth));
+    ether.packetSend(sizeof(broadcastEth));
+
     delay(1000);
 }
