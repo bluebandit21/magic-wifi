510e83803640fb592cb2b9f2fcef288d81e1a09d
Andrew Plotner <bluebandit21@gmail.com>
2022-10-10 18:18:16 -0400

    Finish minimization
    Turns out we only actually needed enc28j60.cpp/enc28j60.h
    
    I suppose that makes sense in a way...
    We really are only using the lowest possible layer here :)

--- dev/ENC28J60_Arduino/test2/EtherCard.cpp
+++
@@ -1,1 +0,0 @@
-../../EtherCard_minimized/src/EtherCard.cpp
--- dev/ENC28J60_Arduino/test2/test2.ino
+++ dev/ENC28J60_Arduino/test2/test2.ino
@@ -1,15 +1,16 @@
-#include "EtherCard.h"
+#include "enc28j60.h"
 
 // ethernet interface mac address, must be unique on the LAN
 static byte mymac[] = { 0x74,0x69,0x69,0x2D,0x30,0x31 };
 
-byte Ethernet::buffer[700];
+ENC28J60 ether;
+byte ENC28J60::buffer[700];
 
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(57600);
     Serial.println("\n[pings]");
-    if (ether.begin(sizeof Ethernet::buffer, mymac, SS) == 0)
+    if (ether.initialize(sizeof Ethernet::buffer, mymac, SS) == 0)
         Serial.println(F("Failed to access Ethernet controller"));
 
     //Enable all the things
@@ -33,14 +34,14 @@ void loop() {
         Serial.println("Received Ethernet packet!");
         Serial.println("Begin hexdump:");
         for(int i=0;i<len;i++){
-            Serial.print(Ethernet::buffer[i], HEX);
+            Serial.print(ENC28J60::buffer[i], HEX);
             Serial.print("  ");
         }
     }
     Serial.println("");
     Serial.println("End hexdump.");
     delay(1000);
-    memcpy(Ethernet::buffer, broadcastEth, sizeof(broadcastEth));
+    memcpy(ENC28J60::buffer, broadcastEth, sizeof(broadcastEth));
     ether.packetSend(sizeof(broadcastEth));
 
     delay(1000);
