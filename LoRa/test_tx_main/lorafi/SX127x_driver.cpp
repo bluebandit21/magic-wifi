0e985739e5d84ccb6e1a211da664ef4fd9bf4554
zaloudan <zaloudan@umich.edu>
2022-10-13 15:39:21 -0400

    Draft of driver file
    Removed Debug

---
+++ LoRa/test_tx_main/lorafi/SX127x_driver.cpp
@@ -0,0 +1,70 @@
+#include <SX127x_driver.h>
+
+uint32_t baseAddress = &SX127X_SPI;
+EUSCI_A_SPI_initMasterParam* sx127x_spi_params;
+uint32_t sx127x_spiFrequency = SX127X_SPI_FREQUENCY;
+int8_t sx127x_nss_port = SX127X_PORT_NSS;
+int8_t sx127x_nss_pin = SX127X_PIN_NSS;
+
+// TODO:: REPLACE with RTOS block function to eliminate busy wait, for TEST use ONLY
+void delay() {
+    for(volatile int i = 0; i < 100000; ++i);
+}
+
+void sx127x_setSPI(EUSCI_A_SPI_initMasterParam &SpiObject)
+{
+    sx127x_spi_params = &SpiObject;
+}
+
+void sx127x_setPins(int8_t nss)
+{
+    // This function is vestigial, MSP will need both port and pin, currently hardcoded //sx127x_nss =  ;
+}
+
+void sx127x_reset(int8_t reset)
+{
+    GPIO_setAsOutputPin(SX127X_PORT_RESET, SX127X_PIN_RESET);
+    GPIO_setOutputHighOnPin(SX127X_PORT_RESET, SX127X_PIN_RESET);
+    delay(1);
+    GPIO_setOutputLowOnPin(SX127X_PORT_RESET, SX127X_PIN_RESET);
+    delay(5);
+}
+
+void sx127x_begin()
+{
+    GPIO_setAsOutputPin(sx127x_nss_port, sx127x_nss_pin);
+    EUSCI_A_SPI_initMaster(baseAddress, sx127x_spi_params);
+    EUSCI_A_SPI_enable(baseAddress);
+}
+
+void sx127x_writeBits(uint8_t address, uint8_t data, uint8_t position, uint8_t length)
+{
+    uint8_t read = sx127x_transfer(address & 0x7F, 0x00);
+    uint8_t mask = (0xFF >> (8 - length)) << position;
+    uint8_t write = (data << position) | (read & ~mask);
+    sx127x_transfer(address | 0x80, write);
+}
+
+void sx127x_writeRegister(uint8_t address, uint8_t data)
+{
+    sx127x_transfer(address | 0x80, data);
+}
+
+uint8_t sx127x_readRegister(uint8_t address)
+{
+    return sx127x_transfer(address & 0x7F, 0x00);
+}
+
+uint8_t sx127x_transfer(uint8_t address, uint8_t data)
+{
+    GPIO_setOutputHighOnPin(sx127x_nss_port, sx127x_nss_pin);
+
+    EUSCI_A_SPI_transmitData(baseAddress, address);
+    EUSCI_A_SPI_transmitData(baseAddress, data);
+    uint8_t response = EUSCI_A_SPI_receiveData(baseAddress); // TODO:: verify this tx/rx pattern works, buffering should make full duplex? 
+    EUSCI_A_SPI_disable(baseAddress); // TODO:: this might be slow ?? Since bus is dedicated, this likely unnecessary
+
+    GPIO_setOutputLowOnPin(sx127x_nss_port, sx127x_nss_pin);
+
+    return response;
+}
