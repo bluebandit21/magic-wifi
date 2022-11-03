964a9f61dccc7f0cf516af3df2abab38ec383c06
zaloudan <zaloudan@umich.edu>
2022-11-03 19:30:30 -0400

    Complete Draft of class to handle subframes

--- LoRa/Full Port/LoRaRF-2.1.1-port/FrameTranslater.cpp
+++ LoRa/Full Port/LoRaRF-2.1.1-port/FrameTranslater.cpp
@@ -16,10 +16,16 @@ FrameTranslater::FrameTranslater(SX127x *send, SX127x *receive) :
 void FrameTranslater::sendFrame(uint8_t* ptr, uint16_t length)
 {
     for(int i = 0; i < (length/lora_frame_max); i++) {
+        // compute parity frame
+#ifdef USE_PARITY
+        for(int j = 0; j < lora_frame_max; j++) {
+            parity_frame[j] ^= ptr[j];
+        }
+#endif
         lora_send->beginPacket();
 
         // packet number
-        lora_send->write(i);
+        lora_send->write(i + (frame_number++ << SUBFRAMES_NUM_MASK));
 
         // message
         lora_send->write(ptr, lora_frame_max);
@@ -27,10 +33,26 @@ void FrameTranslater::sendFrame(uint8_t* ptr, uint16_t length)
         lora_send->write('\0');
         lora_send->endPacket();
         lora_send->wait();
+
+
+
     }
+    // send parity frame if applicable
+    lora_send->beginPacket();
+    // packet number
+    lora_send->write(length/lora_frame_max);
+
+    // message
+    lora_send->write(parity_frame, lora_frame_max);
+    lora_send->write('\0');
+    lora_send->endPacket();
+    lora_send->wait();
+
 }
 
 void FrameTranslater::receiveFrame(uint8_t* dest, uint16_t length){
+    uint8_t * dest_ptr;
+
     // Request for receiving new LoRa packet
     lora_receive->request();
     // Wait for incoming LoRa packet
@@ -40,12 +62,77 @@ void FrameTranslater::receiveFrame(uint8_t* dest, uint16_t length){
     // read() and available() method must be called after request() method
     const uint8_t msgLen = lora_receive->available() - 2; // also skip the header
 
-    // Read header (contains frame number
-    uint8_t subFrameLen = 0;
-    lora_receive->read(&subFrameLen, 1);
+    // Read header (contains frame number)
+    uint8_t subFrameNum = 0;
+    lora_receive->read(&subFrameNum, 1);
+
+    // update segment tracker array
+    active_frames[subFrameNum & SUBFRAMES_NUM_MASK] = subFrameNum &~ SUBFRAMES_NUM_MASK;
+    subFrameNum &= SUBFRAMES_NUM_MASK; // remove packet number for ease of use
+
 
     // read into the appropriate segment of the ethernet frame
-    lora_receive->read(dest + (subFrameLen * lora_frame_max), msgLen);
+    dest_ptr = dest + (subFrameNum * lora_frame_max);
+
+#ifdef USE_PARITY
+    // check if parity frame case
+    if(subFrameNum == MAX_SUBFRAME_ID) {
+        dest_ptr = parity_frame;
+    }
+#endif
+
+    lora_receive->read(dest_ptr, msgLen - 1);
+
+}
+
+
+bool FrameTranslater::checkFrame() {
+    // TODO:: clear the active frame or  something
+    bool parity_needed = false;
+    uint8_t defective_frame;
+
+    uint8_t active_frame_num = active_frames[0];
+
+    // find newest subframe frame
+    for(int i = 1; i < MAX_SUBFRAME_ID - 1; ++i) {
+           if(active_frames[i] > active_frame_num) {
+               active_frame_num = active_frames[i];
+           }
+       }
+
+    // verify all subframes correspond to correct frame
+    for(int i = 1; i < MAX_SUBFRAME_ID - 1; ++i) {
+        if(active_frame_num != active_frames[i]) {
+            // cannot handle more than one bad
+            if(parity_needed) return false;
+            defective_frame = i;
+            parity_needed = true;
+        }
+    }
+
+    if(!parity_needed) return true;
+
+#ifdef USE_PARITY
+    // attempt correction
+    if(active_frame_num != active_frames[MAX_SUBFRAME_ID]) {
+        return false;
+    }
+
+    for(int i = 1; i < MAX_SUBFRAME_ID; ++i) {
+        if(active_frame_num != defective_frame) {
+            for(int j = 0; j < lora_frame_max; ++j) {
+                parity_frame[j] = 0;
+                //parity_frame[j] ^= ptr[j]; // TODO:: fix
+            }
+        }
+    }
+    // return true if successful
+    return true;
+
+#endif
+
+    return false;
+
 
 }
 
--- LoRa/Full Port/LoRaRF-2.1.1-port/FrameTranslater.h
+++ LoRa/Full Port/LoRaRF-2.1.1-port/FrameTranslater.h
@@ -5,20 +5,34 @@
 
 #include "SX127x.h"
 
+#define USE_PARITY
+#define SUBFRAMES_NUM_MASK 0b111 // note this is +1 due to zero index eg. 7 -> 8, must be power of 2
+#define LORA_FRAME_MAX 7
+#define MAX_SUBFRAME_ID 7 // note this is +1 due to zero index eg. 7 -> 8, must be power of 2
+
+
 class FrameTranslater
 {
 
 public:
     FrameTranslater(SX127x *send, SX127x *receive);
     void sendFrame(uint8_t* ptr, uint16_t length);
+
     void receiveFrame(uint8_t* dest, uint16_t length);
+    bool checkFrame();
 
 private:
-    const uint8_t lora_frame_max = 7;
+    const uint8_t lora_frame_max = LORA_FRAME_MAX;
     SX127x* const lora_send;
     SX127x* const lora_receive;
 
+    uint8_t active_frames[8];
+    uint8_t frame_number = 0;
 
-    uint8_t* frame_ptr;
-    uint16_t frame_length;
+
+#ifdef USE_PARITY
+    void parity_fix();
+    uint8_t parity_frame[LORA_FRAME_MAX];
+
+#endif
 };
