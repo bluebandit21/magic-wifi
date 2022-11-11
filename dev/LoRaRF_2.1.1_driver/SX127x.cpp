20c114fec5a9a2362b9ce9187da3d962f70fa444
zaloudan <zaloudan@umich.edu>
2022-11-10 20:25:14 -0500

    merged lora driver into main class

--- dev/LoRaRF_2.1.1_driver/SX127x.cpp
+++ dev/LoRaRF_2.1.1_driver/SX127x.cpp
@@ -683,3 +683,116 @@ void SX127x::onReceive(void(&callback)())
     // register onReceive function to call every receive done
     _onReceive = &callback;
 }
+
+
+// TODO:: REPLACE with RTOS block function to eliminate busy wait, for TEST use ONLY
+void delay(int delay) {
+    for(int i = 0; i < delay; ++i) {
+        for(volatile int j = 0; j < 10; ++j);
+    }
+}
+
+void SX127x::sx127x_setSPI(EUSCI_B_SPI_initMasterParam &SpiObject, LORA port)
+{
+    sx127x_spi_params = SpiObject;
+    baseAddress =     (port == LORA::RECEIVER) ? SX127X_SPI_1 : SX127X_SPI_0;
+    sx127x_nss_port = (port == LORA::RECEIVER) ? SX127X_PORT_1_NSS : SX127X_PORT_0_NSS;
+    sx127x_nss_pin  = (port == LORA::RECEIVER) ? SX127X_PIN_1_NSS : SX127X_PIN_0_NSS;
+
+    sx127x_reset_port = (port == LORA::RECEIVER) ? SX127X_PORT_1_RESET : SX127X_PORT_0_RESET;
+    sx127x_reset_pin  =  (port == LORA::RECEIVER) ? SX127X_PIN_1_RESET : SX127X_PIN_0_RESET;
+}
+
+
+void SX127x::sx127x_reset()
+{
+    GPIO_setOutputHighOnPin(sx127x_reset_port, sx127x_reset_pin);
+    delay(1);
+    GPIO_setOutputLowOnPin(sx127x_reset_port, sx127x_reset_pin);
+    delay(5);
+}
+
+void SX127x::sx127x_begin(LORA port)
+{
+    GPIO_setAsOutputPin(sx127x_nss_port, sx127x_nss_pin);
+    GPIO_setAsOutputPin(sx127x_reset_port, sx127x_reset_pin);
+    GPIO_setOutputHighOnPin(sx127x_nss_port, sx127x_nss_pin);
+
+    if(port == LORA::SENDER){
+        GPIO_setAsInputPinWithPullUpResistor(LORA_SEND_IRQ_PORT, LORA_SEND_IRQ_PIN);
+        GPIO_selectInterruptEdge(LORA_SEND_IRQ_PORT, LORA_SEND_IRQ_PIN, GPIO_LOW_TO_HIGH_TRANSITION);
+    }else{
+        GPIO_setAsInputPinWithPullUpResistor(LORA_RECEIVE_IRQ_PORT, LORA_RECEIVE_IRQ_PIN);
+        GPIO_selectInterruptEdge(LORA_RECEIVE_IRQ_PORT, LORA_RECEIVE_IRQ_PIN, GPIO_LOW_TO_HIGH_TRANSITION);
+    }
+
+    EUSCI_B_SPI_initMaster(baseAddress, &sx127x_spi_params);
+    EUSCI_B_SPI_enable(baseAddress);
+    _enable_interrupt();
+    //EUSCI_B_SPI_clearInterrupt(EUSCI_B0_BASE,
+             // EUSCI_B_SPI_RECEIVE_INTERRUPT);
+        //Enable Receive interrupt
+    //EUSCI_B_SPI_enableInterrupt(EUSCI_B0_BASE,
+              //EUSCI_B_SPI_RECEIVE_INTERRUPT);
+}
+
+void SX127x::sx127x_writeBits(uint8_t address, uint8_t data, uint8_t position, uint8_t length)
+{
+    uint8_t read = sx127x_transfer(address & 0x7F, 0x00);
+    uint8_t mask = (0xFF >> (8 - length)) << position;
+    uint8_t write = (data << position) | (read & ~mask);
+    sx127x_transfer(address | 0x80, write);
+}
+
+void SX127x::sx127x_writeRegister(uint8_t address, uint8_t data)
+{
+    sx127x_transfer(address | 0x80, data);
+}
+
+uint8_t SX127x::sx127x_readRegister(uint8_t address)
+{
+    return sx127x_transfer(address & 0x7F, 0x00);
+}
+
+uint8_t SX127x::sx127x_transfer(uint8_t address, uint8_t data)
+{
+
+    delay(10);
+    GPIO_setOutputLowOnPin(sx127x_nss_port, sx127x_nss_pin);
+    delay(10);
+
+    EUSCI_B_SPI_transmitData(baseAddress, address);
+    EUSCI_B_SPI_transmitData(baseAddress, data);
+    delay(10);
+    uint8_t response = *(volatile uint8_t*)EUSCI_B_SPI_getReceiveBufferAddress(baseAddress);
+
+    delay(10);
+    GPIO_setOutputHighOnPin(sx127x_nss_port, sx127x_nss_pin);
+
+    return response;
+}
+
+void SX127x::sx127x_interruptEnable(LORA port) {
+    switch(port){
+        case LORA::SENDER:
+            GPIO_clearInterrupt(LORA_SEND_IRQ_PORT, LORA_SEND_IRQ_PIN);
+            GPIO_enableInterrupt(LORA_SEND_IRQ_PORT, LORA_SEND_IRQ_PIN);
+            break;
+        case LORA::RECEIVER:
+            GPIO_clearInterrupt(LORA_RECEIVE_IRQ_PORT, LORA_RECEIVE_IRQ_PIN);
+            GPIO_enableInterrupt(LORA_RECEIVE_IRQ_PORT, LORA_RECEIVE_IRQ_PIN);
+            break;
+    }
+}
+
+void SX127x::sx127x_interruptDisable(LORA port) {
+    switch(port){
+        case LORA::SENDER:
+            GPIO_disableInterrupt(LORA_SEND_IRQ_PORT, LORA_SEND_IRQ_PIN);
+            break;
+        case LORA::RECEIVER:
+            GPIO_disableInterrupt(LORA_RECEIVE_IRQ_PORT, LORA_RECEIVE_IRQ_PIN);
+            break;
+    }
+}
+
